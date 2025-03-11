1c283179ee60b5c2d37f905410b847c0dfc2cc76
Lucas <lucastheodoromourofernandes@gmail.com>
2025-03-11 12:55:24 -0400

    Primeiros passos
    Criando um projeto com initializr
    importando o projeto maven no IntelliJ
    Conhecendo a estrutura spring boot
    Bean e CommandLineRunner

---
+++ src/test/java/com/theodorol/primeiros_passos/PrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theodorol.primeiros_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
